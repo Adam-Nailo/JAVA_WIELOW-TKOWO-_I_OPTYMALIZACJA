331bb65d18c7d7f369d8fc7bfda0e0dffc4b1a3d
Adam <155159635+Adam-Nailo@users.noreply.github.com>
2024-04-07 21:47:45 +0200

    Podstawowe operacje na klasie Thread

--- Thread/src/Main.java
+++ Thread/src/Main.java
@@ -1,17 +1,30 @@
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
 
-        Thread thread = new Thread(() -> {
-            for (int i = 0; i < 100; i++) {
-                System.out.println("second thread loop: " + i);
-            }
-        });
-
+        Thread thread = new Thread(() -> secondThread());
+        thread.setName("Second loop thread");
         thread.start();
+        long start = System.currentTimeMillis();
+        mainThread();
+        long end = System.currentTimeMillis();
+        System.out.println("Elapsed time: " + (end - start));
+        Thread thread1 = new Thread(() -> System.out.println("Starting another thread: " + Thread.currentThread().getName()));
+        thread1.setName("No loop thread");
+        thread1.start();
+    }
 
+    private static void mainThread() throws InterruptedException {
+        Thread.sleep(1000);
+        System.out.println("Starting loop in thread: " + Thread.currentThread().getName());
         for (int i = 0; i < 100; i++) {
             System.out.println("main thread loop: " + i);
         }
+    }
 
+    private static void secondThread() {
+        System.out.println("Starting loop in thread: " + Thread.currentThread().getName());
+        for (int i = 0; i < 100; i++) {
+            System.out.println("second thread loop: " + i);
+        }
     }
 }
