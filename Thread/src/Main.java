0efbf610ed00789b7242978cdbc19098ad140e9b
Adam <155159635+Adam-Nailo@users.noreply.github.com>
2024-04-19 23:26:29 +0200

    Callable

--- Thread/src/Main.java
+++ Thread/src/Main.java
@@ -1,3 +1,4 @@
+import callable.Divider;
 import executorservice.ExecutorServiceTask;
 import incrementingvolatileproblem.IncrementingRunnable;
 import incrementingvolatileproblem.ReadingRunnable;
@@ -8,19 +9,34 @@ import synchronizedproblem.SynchronizedCounter;
 import synchronizedproblem.SynchronizedRunnable;
 import waitandnotify.WaitAndNotifyExample;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.ExecutionException;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
 
 public class Main {
     public static void main(String[] args) {
 
-        Dungeon dungeon = new Dungeon(3);
+       ExecutorService service = Executors.newFixedThreadPool(5);
 
-        ExecutorService executorService = Executors.newFixedThreadPool(4);
+        List<Future<Double>> list = new ArrayList<>();
 
-        for (int i = 0; i < 4; i++) {
-            executorService.submit(() -> dungeon.tryToEnter());
-        }
+       for (int i =0;i<5;i++){
+           Future<Double> result = service.submit(new Divider((double) (i * 5)));
+           list.add(result);
+       }
+
+       for(Future<Double> result: list){
+           try {
+               System.out.println(result.get());
+           } catch (InterruptedException e) {
+               throw new RuntimeException(e);
+           } catch (ExecutionException e) {
+               throw new RuntimeException(e);
+           }
+       }
 
     }
 
---
+++ Thread/src/callable/Divider.java
@@ -0,0 +1,20 @@
+package callable;
+
+import java.util.concurrent.Callable;
+
+public class Divider implements Callable<Double> {
+
+    private Double number;
+
+    public Divider(Double number) {
+        this.number = number;
+    }
+
+    @Override
+    public Double call() throws Exception {
+        System.out.println("Entering call method, thread " + Thread.currentThread().getName());
+        Thread.sleep(1000);
+        System.out.println("Leaving call method, thread " + Thread.currentThread().getName());
+        return number / 2;
+    }
+}
