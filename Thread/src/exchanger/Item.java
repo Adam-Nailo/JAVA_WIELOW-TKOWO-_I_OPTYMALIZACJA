836f747fb348f3b0b16078390b11fb157b5a5a7e
Adam <155159635+Adam-Nailo@users.noreply.github.com>
2024-04-28 22:53:16 +0200

    Exchanger v.1

---
+++ Thread/src/exchanger/Item.java
@@ -0,0 +1,33 @@
+package exchanger;
+
+public class Item {
+
+    private String name;
+    private int modifier;
+
+    public Item(String name, int modifier) {
+        this.name = name;
+        this.modifier = modifier;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getModifier() {
+        return modifier;
+    }
+
+    public void setModifier(int modifier) {
+        this.modifier = modifier;
+    }
+
+    @Override
+    public String toString() {
+        return name + " +" + modifier;
+    }
+}
