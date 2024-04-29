f7051780d47437edfd175f0ea6a278120368648a
Adam <155159635+Adam-Nailo@users.noreply.github.com>
2024-04-30 00:01:16 +0200

    Exchanger v.3

--- Thread/src/Main.java
+++ Thread/src/Main.java
@@ -4,6 +4,9 @@ import concurrenthashmap.AddingItemsRunnable;
 import concurrenthashmap.GettingItemsRunnable;
 import cyclicbarrier.CyclicBarrierRunnable;
 import delayqueue.DelayedMission;
+import exchanger.BlacksmithRunnable;
+import exchanger.Item;
+import exchanger.WarriorRunnable;
 import priorityblockingqueue.Case;
 import priorityblockingqueue.CaseRunnable;
 import priorityblockingqueue.HospitalRunnable;
@@ -13,10 +16,9 @@ import java.util.concurrent.*;
 public class Main {
     public static void main(String[] args) {
         ExecutorService service = Executors.newFixedThreadPool(2);
+        Exchanger<Item> itemExchanger = new Exchanger<>();
 
-        ConcurrentMap<Integer,String> map = new ConcurrentHashMap<>();
-
-        service.execute(new AddingItemsRunnable(map));
-        service.execute(new GettingItemsRunnable(map));
+        service.execute(new WarriorRunnable(itemExchanger, new Item("Basic armor",0)));
+        service.execute(new BlacksmithRunnable(itemExchanger, new Item("Reinforced armor",2)));
     }
 }
---
+++ Thread/src/exchanger/BlacksmithRunnable.java
@@ -0,0 +1,30 @@
+package exchanger;
+
+import java.util.concurrent.Exchanger;
+
+public class BlacksmithRunnable implements Runnable {
+    private Exchanger<Item> exchanger;
+    private Item armor;
+
+    public BlacksmithRunnable(Exchanger<Item> exchanger, Item armor) {
+        this.exchanger = exchanger;
+        this.armor = armor;
+    }
+
+    @Override
+    public void run() {
+        while (true) {
+            try {
+                System.out.println("Blacksmith has " + armor);
+                Item exchangedItem = exchanger.exchange(armor);
+                System.out.println("Blacksmith recived given item: " + exchangedItem);
+                exchangedItem.setModifier(exchangedItem.getModifier() + 2);
+                armor = exchangedItem;
+                Thread.sleep(4000);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        }
+
+    }
+}
