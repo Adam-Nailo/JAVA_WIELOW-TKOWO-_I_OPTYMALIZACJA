490f8b45a95d589c1701b81dda3d5a39f393f40c
Adam <155159635+Adam-Nailo@users.noreply.github.com>
2024-05-07 22:54:17 +0200

    PersonalDataService v.2

--- optimizing_latency/src/main/java/org/example/Main.java
+++ optimizing_latency/src/main/java/org/example/Main.java
@@ -2,13 +2,12 @@ package org.example;
 
 public class Main {
     private static PathManager pathManager = new PathManager();
+    private static PersonalDataService personalDataService = new PersonalDataService();
 
     public static void main(String[] args) {
-        String line = "Barnaby,Valance,bvalance0@intel.com,Male";
 
-        CalculatedPersonalDataDto dto = new CalculatedPersonalDataDto();
-        dto.calculateLine(line);
+       CalculatedPersonalDataDto calculatedPersonalData =  personalDataService.getCalculatedPersonalData(pathManager.createAbsoluteResourceFilePath("data1.txt"));
 
-        System.out.println(dto);
+        System.out.println(calculatedPersonalData);
     }
 }
--- optimizing_latency/src/main/java/org/example/PersonalDataService.java
+++ optimizing_latency/src/main/java/org/example/PersonalDataService.java
@@ -1,4 +1,29 @@
 package org.example;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
 public class PersonalDataService {
+
+    public CalculatedPersonalDataDto getCalculatedPersonalData(String filepath) {
+        try (BufferedReader reader = new BufferedReader(new FileReader(filepath))) {
+            return readAndCalculateLines(reader);
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private CalculatedPersonalDataDto readAndCalculateLines(BufferedReader reader) throws IOException {
+        CalculatedPersonalDataDto dto = new CalculatedPersonalDataDto();
+        String line = reader.readLine();
+        while (line != null) {
+            dto.calculateLine(line);
+            line = reader.readLine();
+        }
+        return dto;
+    }
 }
