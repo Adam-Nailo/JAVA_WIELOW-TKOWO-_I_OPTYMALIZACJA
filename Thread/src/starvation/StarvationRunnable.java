d1da123e5f788f8ce997baad9c6ddb0c3f428153
Adam <155159635+Adam-Nailo@users.noreply.github.com>
2024-05-14 23:40:23 +0200

    Starvation v.2

--- Thread/src/starvation/StarvationRunnable.java
+++ Thread/src/starvation/StarvationRunnable.java
@@ -1,4 +1,10 @@
 package starvation;
 
 public class StarvationRunnable {
+
+    private CommonResource commonResource;
+
+    public StarvationRunnable(CommonResource commonResource) {
+        this.commonResource = commonResource;
+    }
 }
