d97b5502b3e25128359fedb5a623d07e8af7eb3d
Adam <155159635+Adam-Nailo@users.noreply.github.com>
2024-05-21 23:39:22 +0200

    Symulacja v.3

--- DiningPhilosophers/src/main/java/diningphilosophers/Main.java
+++ DiningPhilosophers/src/main/java/diningphilosophers/Main.java
@@ -3,6 +3,8 @@ package diningphilosophers;
 public class Main {
 
     private static final int NUMBER_OF_PHILOSOPHERS = 5;
+    private static final int NUMBER_OF_CHOPSTICKS = 5;
+    private static final int SIMULATION_TIME = 5;
     public static void main(String[] args) {
 
     }
