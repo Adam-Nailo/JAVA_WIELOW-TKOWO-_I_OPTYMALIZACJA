dcc2f18bdb54886e88b8652a340a66f268620512
Adam <155159635+Adam-Nailo@users.noreply.github.com>
2024-05-22 23:35:00 +0200

    Symulacja v.4

--- DiningPhilosophers/src/main/java/diningphilosophers/Main.java
+++ DiningPhilosophers/src/main/java/diningphilosophers/Main.java
@@ -4,7 +4,7 @@ public class Main {
 
     private static final int NUMBER_OF_PHILOSOPHERS = 5;
     private static final int NUMBER_OF_CHOPSTICKS = 5;
-    private static final int SIMULATION_TIME = 5;
+    private static final int SIMULATION_TIME = 6000;
     public static void main(String[] args) {
 
     }
