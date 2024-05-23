f52ff2807856a2eb8d1d07f2f79d8ee144dbca11
Adam <155159635+Adam-Nailo@users.noreply.github.com>
2024-05-23 23:31:40 +0200

    Symulacja v.5

--- DiningPhilosophers/src/main/java/diningphilosophers/Main.java
+++ DiningPhilosophers/src/main/java/diningphilosophers/Main.java
@@ -7,5 +7,9 @@ public class Main {
     private static final int SIMULATION_TIME = 6000;
     public static void main(String[] args) {
 
+        ExecutorService service = new ExecutorService(){
+
+        }
+
     }
 }
